d7c6a9fdf9969b606b94b3daef57885214a5c10d
Tobias Kliem <tobias.kliem@gmail.com>
2013-11-06 23:10:57 +0000

    Finished the form and the template, still need to set up the class instances. Also made some changes to Year View and added telephone number to views

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from scheduler.models import *
+from database.models import Student
 
 class AppointmentForm(forms.Form):
-   LENGTHS = ( 
+    LENGTHS = ( 
        (5, '5 Minutes'),
        (10, '10 Minutes'),
        (15, '15 Minutes'),
@@ -16,79 +17,62 @@ class AppointmentForm(forms.Form):
        (55, '55 Minutes'),
        (60, '1 Hour')
        )
-    message = forms.TextField(attrs = {
+    message = forms.CharField(widget = forms.Textarea(attrs = {
         'class': 'form-control', 'rows': '6', 
         'placeholder': 'Any Details you want to tell the students - what they need to bring or prepare, location of the meeting etc'
-        })
-    slot_1_date = forms.DateField(attrs = {
-        'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'
-        })
-    slot_1_from = forms.TimeField(attrs = {
+        }))
+    slot_1_date = forms.DateField(widget = forms.TextInput(attrs = {
+        'data-date-format': 'dd/mm/yyyy', 'class': 'form-control slot_date', 'placeholder': 'Date for the meetings'
+        }))
+    slot_1_from = forms.TimeField(widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'Start time in 24h format (eg 14:30)'
-        })
-    slot_1_until = forms.TimeField(attrs = {
+        }))
+    slot_1_until = forms.TimeField(widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'End time in 24h format (eg 18:00)'
-        })
-    slot_2_date = forms.DateField(attrs = {
-        'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'
-        }, required = False)
-    slot_2_from = forms.TimeField(attrs = {
+        }))
+    slot_2_date = forms.DateField(required = False, widget = forms.TextInput(attrs = {
+        'data-date-format': 'dd/mm/yyyy', 'class': 'form-control slot_date', 'placeholder': 'Date for the meetings'
+        }))
+    slot_2_from = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'Start time in 24h format (eg 14:30)'
-        }, required = False)
-    slot_2_until = forms.TimeField(attrs = {
+        }))
+    slot_2_until = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'End time in 24h format (eg 18:00)'
-        }, required = False)
-    slot_3_date = forms.DateField(attrs = {
-        'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'
-        }, required = False)
-    slot_3_from = forms.TimeField(attrs = {
+        }))
+    slot_3_date = forms.DateField(required = False, widget = forms.TextInput(attrs = {
+        'data-date-format': 'dd/mm/yyyy', 'class': 'form-control slot_date', 'placeholder': 'Date for the meetings'
+        }))
+    slot_3_from = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'Start time in 24h format (eg 14:30)'
-        }, required = False)
-    slot_3_until = forms.TimeField(attrs = {
+        }))
+    slot_3_until = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'End time in 24h format (eg 18:00)'
-        }, required = False)
-    slot_4_date = forms.DateField(attrs = {
-        'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'
-        }, required = False)
-    slot_4_from = forms.TimeField(attrs = {
+        }))
+    slot_4_date = forms.DateField(required = False, widget = forms.TextInput(attrs = {
+        'data-date-format': 'dd/mm/yyyy', 'class': 'form-control slot_date', 'placeholder': 'Date for the meetings'
+        }))
+    slot_4_from = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'Start time in 24h format (eg 14:30)'
-        }, required = False)
-    slot_4_until = forms.TimeField(attrs = {
+        }))
+    slot_4_until = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'End time in 24h format (eg 18:00)'
-        }, required = False)
-    slot_5_date = forms.DateField(attrs = {
-        'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'
-        }, required = False)
-    slot_5_from = forms.TimeField(attrs = {
+        }))
+    slot_5_date = forms.DateField(required = False, widget = forms.TextInput(attrs = {
+        'data-date-format': 'dd/mm/yyyy', 'class': 'form-control slot_date', 'placeholder': 'Date for the meetings'
+        }))
+    slot_5_from = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'Start time in 24h format (eg 14:30)'
-        }, required = False)
-    slot_5_until = forms.TimeField(attrs = {
+        }))
+    slot_5_until = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'End time in 24h format (eg 18:00)'
-        }, required = False)
-    slot_6_date = forms.DateField(attrs = {
-        'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'
-        }, required = False)
-    slot_6_from = forms.TimeField(attrs = {
+        }))
+    slot_6_date = forms.DateField(required = False, widget = forms.TextInput(attrs = {
+        'data-date-format': 'dd/mm/yyyy', 'class': 'form-control slot_date', 'placeholder': 'Date for the meetings'
+        }))
+    slot_6_from = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'Start time in 24h format (eg 14:30)'
-        }, required = False)
-    slot_6_until = forms.TimeField(attrs = {
+        }))
+    slot_6_until = forms.TimeField(required = False, widget = forms.TextInput(attrs = {
         'class': 'form-control input-sm', 'placeholder': 'End time in 24h format (eg 18:00)'
-        }, required = False)
-    appointment_length = forms.ChoiceField(choices = LENGHTS)
-
-
-
-
-#class AppointmentForm(forms.ModelForm):
-#
-#    class Meta:
-#        model = student_teacher_appointment
-#        widgets = {
-#            'start_time': forms.,
-#            #      'date_of_meet': forms.DateInput(attrs={'data-datetime-format': 'dd/mm/yyyy', 'class': 'form-control'}), #Check how to
-#            #'end_time': forms.DateTimeField()
-#            'student': forms.CheckboxSelectMultiple,
-#            teacher = models.ForeignKey(User)
-#            invited = models.ManyToManyField(Student, blank=True, null=True)
-#                }
-#
+        }))
+    appointment_length = forms.ChoiceField(choices = LENGTHS)
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,45 +1,82 @@
+import datetime
+import time
+
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import Template, Context, RequestContext
 
 from database.models import Student, Module, MetaData
 from database.views import is_admin, is_student, is_teacher
 from scheduler.models import *
+from scheduler.forms import *
 
 @login_required
 @user_passes_test(is_teacher)
 def set_up_appointments(request):
     errors = []
     if request.method == 'POST':
-        invitees = request.POST.getlist('selected_student_id')
-        if len(invitees) == 0:
-            errors.append("No students selected")
-        else:
-            date_1 = request.POST['slot_1_date']
-            if  date_1 == None:
-                errors.append("Specify at least one date")
-            elif len(date_1.split('/')) != 3:
-                errors.append("Enter a valid date")
-            else:
-                time_from = request.POST['slot_1_from'].split(':')
-                hour_from = time_from[0]
-                minute_from = time_from[1]
-
-                
-
-
-
-
-            appointment = StudentTeacherAppointment(
-                )
-
-        return HttpResponseRedirect()
-
+        form = AppointmentForm(request.POST)
+        invitees = request.POST.getlist('selected_student_id') # Do Validation for these in Javascript
+        if form.is_valid():
+            message = form.cleaned_data['message']
+            minutes = int(form.cleaned_data['appointment_length'])
+            length = datetime.timedelta(minutes=minutes)
+            slots = []
+            slot = {}
+            slot['date'] = form.cleaned_data['slot_1_date']
+            slot['from'] = form.cleaned_data['slot_1_from']
+            slot['until'] = form.cleaned_data['slot_1_until']
+            slots.append(slot)
+            if form.cleaned_data['slot_2_date']:
+                slot = {}
+                slot['date'] = form.cleaned_data['slot_2_date']
+                slot['from'] = form.cleaned_data['slot_2_from']
+                slot['until'] = form.cleaned_data['slot_2_until']
+                slots.append(slot)
+            if form.cleaned_data['slot_3_date']:
+                slot = {}
+                slot['date'] = form.cleaned_data['slot_3_date']
+                slot['from'] = form.cleaned_data['slot_3_from']
+                slot['until'] = form.cleaned_data['slot_3_until']
+                slots.append(slot)
+            if form.cleaned_data['slot_4_date']:
+                slot = {}
+                slot['date'] = form.cleaned_data['slot_4_date']
+                slot['from'] = form.cleaned_data['slot_4_from']
+                slot['until'] = form.cleaned_data['slot_4_until']
+                slots.append(slot)
+            if form.cleaned_data['slot_5_date']:
+                slot = {}
+                slot['date'] = form.cleaned_data['slot_5_date']
+                slot['from'] = form.cleaned_data['slot_5_from']
+                slot['until'] = form.cleaned_data['slot_5_until']
+                slots.append(slot)
+            if form.cleaned_data['slot_6_date']:
+                slot = {}
+                slot['date'] = form.cleaned_data['slot_6_date']
+                slot['from'] = form.cleaned_data['slot_6_from']
+                slot['until'] = form.cleaned_data['slot_6_until']
+                slots.append(slot)
+            for slot in slots:
+                date = slot['date']
+                slot_start = datetime.datetime.combine(date, slot['from'])
+                slot_end = datetime.datetime.combine(date, slot['until'])
+                start = slot_start
+                while start + length < slot_end:
+                    print "From:"
+                    print start.time()
+                    start += length
+                    print "Until:"
+                    print start.time()
+                    print "---"
+            return HttpResponseRedirect('/set_up_appointments')
+    else:
+        form = AppointmentForm(initial={'appointment_length': 15})
     all_students = Student.objects.filter(active=True)
     meta_stuff = MetaData.objects.get(data_id=1)
     current_year = meta_stuff.current_year
-#    this_years_modules = Module.objects.filter(instructors__in = request.user, year=current_year)
     this_years_modules = request.user.module_set.all()
     rows = []
     current_modules = {} #Only pass the strings for the ones with students on
@@ -58,15 +95,10 @@ def set_up_appointments(request):
                 module_title += "/" + str(module.year + 1)
                 if module_no_space not in current_modules:
                     current_modules[module_no_space] = module_title
-                            #                module_string = module.title.replace(" ", "_")
-                            #                module_string += "_" + str(module.year)
-                            #                modules += " " + module_string
-                            #                if module_string not in current_modules:
-                            #                    current_modules.append(module_string)
         entry['modules'] = modules
         rows.append(entry)
 
     return render_to_response('set_up_appointment.html',
-            {'rows': rows, 'current_modules': current_modules},
+            {'rows': rows, 'current_modules': current_modules, 'form': form},
             context_instance=RequestContext(request)
         )
