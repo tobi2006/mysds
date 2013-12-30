bfdc420644db5a958d1d0eaeadd5ba83b1c2e19d
Tobias Kliem <tobias.kliem@gmail.com>
2013-12-30 11:03:11 +0000

    Housekeeping

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+ssh -t tobiaskliem.de "/home/tobi/bin/backup_db.sh"
 rsync -ave ssh --exclude-from 'exclude.txt' . tobi@tobiaskliem.de:/home/tobi/cccu
 ssh -t tobiaskliem.de "rm database/*.pyc; rm mysds/*.pyc; rm export/*.pyc; rm anonymous_marking/*.pyc; sudo service apache2 restart"
