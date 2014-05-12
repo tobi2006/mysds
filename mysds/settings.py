583b479b2604a9ab6b4773d564f189b93e721607
Tobias Kliem <tobias.kliem@gmail.com>
2014-05-12 21:55:11 +0100

    included south

--- mysds/settings.py
+++ mysds/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'anonymous_marking',
     'feedback',
     'announcer',
+    'south',
     #    'scheduler'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
