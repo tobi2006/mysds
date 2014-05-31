8d4873e9a4b6e89cb0818706961e67758c799f2e
Tobias Kliem <tobias.kliem@gmail.com>
2014-05-31 17:42:24 +0100

    Made announcer app PEP8 compliant

--- announcer/models.py
+++ announcer/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Announcement(models.Model):
     ANNOUNCE_TO = (
-            ('students', 'Students'),
-            ('teachers', 'Staff'),
-            ('all', 'All')
-            )
+        ('students', 'Students'),
+        ('teachers', 'Staff'),
+        ('all', 'All')
+        )
 
     author = models.ForeignKey(User)
     publishing_date = models.DateTimeField()
-    text = models.TextField(blank = True, null = True)
-    headline = models.CharField(max_length = 200, blank = True, null = True)
-    announce_to = models.CharField(max_length = 20, choices = ANNOUNCE_TO)
-
+    text = models.TextField(blank=True, null=True)
+    headline = models.CharField(max_length=200, blank=True, null=True)
+    announce_to = models.CharField(max_length=20, choices=ANNOUNCE_TO)
 
     class Meta:
         ordering = ['-publishing_date', 'author']
--- announcer/views.py
+++ announcer/views.py
@@ -8,19 +8,23 @@ from announcer.models import *
 from database.strings import *
 from database.views import is_teacher, is_student, is_admin
 
+
 @login_required
 def show_homepage(request):
     announcements = []
     admin = False
     if is_student(request.user):
         print "Whoohoo"
-        announcements = Announcement.objects.filter(Q(announce_to='students') | Q(announce_to='all'))
+        announcements = Announcement.objects.filter(
+            Q(announce_to='students') | Q(announce_to='all'))
     else:
         if is_admin(request.user):
-            announcements = Announcement.objects.filter(Q(announce_to='teachers') | Q(announce_to='all'))
+            announcements = Announcement.objects.filter(
+                Q(announce_to='teachers') | Q(announce_to='all'))
             admin = True
         elif is_teacher(request.user):
-            announcements = Announcement.objects.filter(Q(announce_to='teachers') | Q(announce_to='all'))
+            announcements = Announcement.objects.filter(
+                Q(announce_to='teachers') | Q(announce_to='all'))
 
     if request.method == 'POST':
         if request.POST['header'] or request.POST['text']:
@@ -33,15 +37,19 @@ def show_homepage(request):
             else:
                 text = ''
             new_announcement = Announcement(
-                    author = request.user,
-                    publishing_date = datetime.datetime.today(),
-                    headline = header,
-                    announce_to = request.POST['announce_to'],
-                    text = text
-                    )
+                author=request.user,
+                publishing_date=datetime.datetime.today(),
+                headline=header,
+                announce_to=request.POST['announce_to'],
+                text=text
+                )
             new_announcement.save()
-    
-    return render_to_response('home.html',
-            {'announcements': announcements, 'mdexplanation': MD_EXPLANATION, 'admin': admin},
-            context_instance = RequestContext(request)
+    return render_to_response(
+        'home.html',
+        {
+            'announcements': announcements,
+            'mdexplanation': MD_EXPLANATION,
+            'admin': admin
+        },
+        context_instance=RequestContext(request)
         )
