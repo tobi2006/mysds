17edb8ac38a5912211304002f2b56d517f6a28ac
Tobias Kliem <tobias.kliem@gmail.com>
2014-06-04 09:57:41 +0100

    Made main app PEP8 compliant

--- mysds/urls.py
+++ mysds/urls.py
@@ -1,96 +1,352 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-#from django.contrib.auth.views import login, logout
-#from django.contrib import auth
+# from django.contrib.auth.views import login, logout
+# from django.contrib import auth
 import mysds
 import database
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='logout'),
-    url(r'^accounts/change_password/$', 'django.contrib.auth.views.password_change', {'template_name': 'change_password.html'}, name='change_password'),
-    url(r'^accounts/change_password_done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'password_change_done.html'}, name='change_password_done')
+urlpatterns = patterns(
+    '',
+    url(
+        r'^accounts/login/$',
+        'django.contrib.auth.views.login',
+        {'template_name': 'login.html'},
+        name='login'
+        ),
+    url(
+        r'^accounts/logout/$',
+        'django.contrib.auth.views.logout',
+        {'template_name': 'logout.html'},
+        name='logout'
+        ),
+    url(
+        r'^accounts/change_password/$',
+        'django.contrib.auth.views.password_change',
+        {'template_name': 'change_password.html'},
+        name='change_password'
+        ),
+    url(
+        r'^accounts/change_password_done/$',
+        'django.contrib.auth.views.password_change_done',
+        {'template_name': 'password_change_done.html'},
+        name='change_password_done'
+        )
 )
 
-urlpatterns += patterns('announcer.views',
-    url(r'^$', 'show_homepage', name = 'home')
+urlpatterns += patterns(
+    'announcer.views',
+    url(r'^$', 'show_homepage', name='home')
 )
 
-urlpatterns += patterns('mysds.views',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^na/$', 'na', name = 'na'),
-    url(r'^remove_student_from_module/(\w+)/(\d{4})/$', 'na', name = 'generic_remove_student_from_module'),
-    url(r'^reset_password/$', 'reset_password', name = 'reset_password'),
-    url(r'^invite_students/$', 'invite_students', name = 'invite_students'),
-    url(r'^wrong_email/$', 'wrong_email', name = 'wrong_email'),
-    #Dummies
-    url(r'^all_attendances/$', 'na', name = 'all_attendances_year'), #Dummy to enable url method
-    url(r'^all_tutees/$', 'na', name = 'all_tutees_year'), #Dummy to enable url method
-    url(r'^attendance/(\w+)/(\d{4})/$', 'na', name = 'attendance_no'), #Dummy to enable url method
-    url(r'^delete_tutee_meeting/$', 'na', name='delete_tutee_meeting_no'), #Dummy to enable url method
-    url(r'^de_anonymize/(\w+)/(\d{4})/$', 'na', name = 'de_anonymize_no'), # Dummy to enable url method
-    url(r'^export_all_anonymous_exam_marks/$', 'na', name='export_anonymous_exam_marks_year'), #Dummy to enable url method
-    url(r'^export_anonymous_marks/(\w+)/(\d{4})/$', 'na', name='export_anonymous_marks_no'), #Dummy to enable url method
-    url(r'^mark/(\w+)/(\d{4})/$', 'na', name = 'mark_no'), #Dummy to enable url method
-    url(r'^mark_anonymously/(\w+)/(\d{4})/$', 'na', name = 'mark_anon'), #Dummy to enable url method
-    url(r'^toggle_assessment_availability/(\w+)/(\d{4})/$', 'na', name = 'toggle_assessment'), #Dummy to enable url method
+urlpatterns += patterns(
+    'mysds.views',
+    url(
+        r'^admin/',
+        include(admin.site.urls)
+        ),
+    url(r'^na/$',
+        'na',
+        name='na'
+        ),
+    url(
+        r'^remove_student_from_module/(\w+)/(\d{4})/$',
+        'na',
+        name='generic_remove_student_from_module'
+        ),
+    url(
+        r'^reset_password/$',
+        'reset_password',
+        name='reset_password'
+        ),
+    url(
+        r'^invite_students/$',
+        'invite_students',
+        name='invite_students'
+        ),
+    url(
+        r'^wrong_email/$',
+        'wrong_email',
+        name='wrong_email'
+        ),
+    url(
+        r'^all_attendances/$',
+        'na',
+        name='all_attendances_year'
+        ),
+    url(
+        r'^all_tutees/$',
+        'na',
+        name='all_tutees_year'
+        ),
+    url(
+        r'^attendance/(\w+)/(\d{4})/$',
+        'na',
+        name='attendance_no'
+        ),
+    url(
+        r'^delete_tutee_meeting/$',
+        'na',
+        name='delete_tutee_meeting_no'
+        ),
+    url(
+        r'^de_anonymize/(\w+)/(\d{4})/$',
+        'na',
+        name='de_anonymize_no'
+        ),
+    url(
+        r'^export_all_anonymous_exam_marks/$',
+        'na',
+        name='export_anonymous_exam_marks_year'
+        ),
+    url(
+        r'^export_anonymous_marks/(\w+)/(\d{4})/$',
+        'na',
+        name='export_anonymous_marks_no'
+        ),
+    url(
+        r'^mark/(\w+)/(\d{4})/$',
+        'na',
+        name='mark_no'
+        ),
+    url(
+        r'^mark_anonymously/(\w+)/(\d{4})/$',
+        'na',
+        name='mark_anon'
+        ),
+    url(
+        r'^toggle_assessment_availability/(\w+)/(\d{4})/$',
+        'na',
+        name='toggle_assessment'
+        ),
 )
 
-urlpatterns += patterns('database.views',
-    url(r'^add_module/$', 'add_module', name='add_module'),
-    url(r'^add_student/$', 'add_student', name='add_student'),
-    url(r'^add_students_to_module/(\w+)/(\d{4})/$', 'add_students_to_module', name='add_students_to_module'),
-    url(r'^address_nines/(\w+)/(\d{4})/$', 'address_nines', name = 'address_nines'),
-    url(r'^all_attendances/(\w+)/$', 'all_attendances', name = 'all_attendances'),
-    url(r'^all_tutees/(\w+)/$', 'all_tutees', name = 'tutees'),
-    url(r'^attendance/(\w+)/(\d{4})/(\w+)/$', 'attendance', name = 'attendance'),
-    url(r'^delete_module/(\w+)/(\d{4})/$', 'delete_module', name = 'delete_module'),
-    url(r'^delete_tutee_meeting/(\d+)/$', 'delete_tutee_meeting', name='delete_tutee_meeting'),
-    url(r'^edit_module/(\w+)/(\d{4})/$', 'edit_module', name='edit_module'),
-    url(r'^edit_student/(\w+)/$', 'edit_student', name='edit_student'),
-    url(r'^import_success/$', 'import_success', name = 'import_success'),
-    url(r'^lsp_edit/(\w+)/$', 'lsp_edit', name='lsp_edit'),
-    url(r'^lsp_view/(\w+)/$', 'lsp_view', name='lsp_view'),
-    url(r'^mark/(\w+)/(\d{4})/(\w+)/$', 'mark', name = 'mark'),
-    url(r'^module/(\w+)/(\d{4})/$', 'module_view', name = 'module_view'),
-    url(r'^notes_edit/(\w+)/$', 'notes_edit', name='notes_edit'),
-    url(r'^parse_csv/$', 'parse_csv', name = 'parse_csv'),
-    url(r'^assessment_groups/(\w+)/(\d{4})/$', 'assessment_groups', name = 'assessment_groups'),
-    url(r'^remove_student_from_module/(\w+)/(\d{4})/(\w+)/$', 'remove_student_from_module', name = 'remove_student_from_module'),
-    url(r'^search_student/$', 'search_student', name = 'search_student'),
-    url(r'^seminar_group_overview/(\w+)/(\d{4})/$', 'seminar_group_overview', name='seminar_group_overview'),
-    url(r'^seminar_groups/(\w+)/(\d{4})/$', 'seminar_groups', name = 'seminar_groups'),
-    url(r'^student/(\w+)/$', 'student_view', name = 'student_view'),
-    url(r'^student/(\w+)/(\d+)/$', 'student_view', name = 'edit_tutee_meeting'),
-    url(r'^student_marks/$', 'student_marks', name='student_marks'),
-    url(r'^toggle_assessment_availability/(\w+)/(\d{4})/(\d{1})/$', 'toggle_assessment_availability', name = 'toggle_assessment_availability'),
-    url(r'^tutee_list/$', 'tutee_list', name='tutee_list'),
-    url(r'^upload_csv/$', 'upload_csv', name = 'upload_csv'),
-    url(r'^year/(\w+)/$', 'year_view', name = 'year_view'),
+urlpatterns += patterns(
+    'database.views',
+    url(
+        r'^add_module/$',
+        'add_module',
+        name='add_module'
+        ),
+    url(
+        r'^add_student/$',
+        'add_student',
+        name='add_student'
+        ),
+    url(
+        r'^add_students_to_module/(\w+)/(\d{4})/$',
+        'add_students_to_module',
+        name='add_students_to_module'
+        ),
+    url(
+        r'^address_nines/(\w+)/(\d{4})/$',
+        'address_nines',
+        name='address_nines'
+        ),
+    url(
+        r'^all_attendances/(\w+)/$',
+        'all_attendances',
+        name='all_attendances'
+        ),
+    url(
+        r'^all_tutees/(\w+)/$',
+        'all_tutees',
+        name='tutees'
+        ),
+    url(
+        r'^attendance/(\w+)/(\d{4})/(\w+)/$',
+        'attendance',
+        name='attendance'
+        ),
+    url(
+        r'^delete_module/(\w+)/(\d{4})/$',
+        'delete_module',
+        name='delete_module'
+        ),
+    url(
+        r'^delete_tutee_meeting/(\d+)/$',
+        'delete_tutee_meeting',
+        name='delete_tutee_meeting'
+        ),
+    url(
+        r'^edit_module/(\w+)/(\d{4})/$',
+        'edit_module',
+        name='edit_module'
+        ),
+    url(
+        r'^edit_student/(\w+)/$',
+        'edit_student',
+        name='edit_student'
+        ),
+    url(
+        r'^import_success/$',
+        'import_success',
+        name='import_success'
+        ),
+    url(
+        r'^lsp_edit/(\w+)/$',
+        'lsp_edit',
+        name='lsp_edit'
+        ),
+    url(
+        r'^lsp_view/(\w+)/$',
+        'lsp_view',
+        name='lsp_view'
+        ),
+    url(
+        r'^mark/(\w+)/(\d{4})/(\w+)/$',
+        'mark',
+        name='mark'
+        ),
+    url(
+        r'^module/(\w+)/(\d{4})/$',
+        'module_view',
+        name='module_view'
+        ),
+    url(
+        r'^notes_edit/(\w+)/$',
+        'notes_edit',
+        name='notes_edit'
+        ),
+    url(
+        r'^parse_csv/$',
+        'parse_csv',
+        name='parse_csv'
+        ),
+    url(
+        r'^assessment_groups/(\w+)/(\d{4})/$',
+        'assessment_groups',
+        name='assessment_groups'
+        ),
+    url(
+        r'^remove_student_from_module/(\w+)/(\d{4})/(\w+)/$',
+        'remove_student_from_module',
+        name='remove_student_from_module'
+        ),
+    url(
+        r'^search_student/$',
+        'search_student',
+        name='search_student'
+        ),
+    url(
+        r'^seminar_group_overview/(\w+)/(\d{4})/$',
+        'seminar_group_overview',
+        name='seminar_group_overview'
+        ),
+    url(
+        r'^seminar_groups/(\w+)/(\d{4})/$',
+        'seminar_groups',
+        name='seminar_groups'
+        ),
+    url(
+        r'^student/(\w+)/$',
+        'student_view',
+        name='student_view'
+        ),
+    url(
+        r'^student/(\w+)/(\d+)/$',
+        'student_view',
+        name='edit_tutee_meeting'
+        ),
+    url(
+        r'^student_marks/$',
+        'student_marks',
+        name='student_marks'
+        ),
+    url(
+        r'^toggle_assessment_availability/(\w+)/(\d{4})/(\d{1})/$',
+        'toggle_assessment_availability',
+        name='toggle_assessment_availability'
+        ),
+    url(
+        r'^tutee_list/$',
+        'tutee_list',
+        name='tutee_list'
+        ),
+    url(
+        r'^upload_csv/$',
+        'upload_csv',
+        name='upload_csv'
+        ),
+    url(
+        r'^year/(\w+)/$',
+        'year_view',
+        name='year_view'
+        ),
 )
 
-urlpatterns += patterns('export.views',
-    url(r'^export_all_anonymous_exam_marks/(\d+)/$', 'export_all_anonymous_exam_marks', name = 'export_all_anonymous_exam_marks'),
-    url(r'^export_anonymous_marks/(\w+)/(\d+)/(\w+)$', 'export_anonymous_marks', name = 'export_anonymous_marks'),
-    url(r'^export_attendance_sheet/(\w+)/(\d+)/$', 'export_attendance_sheet', name = 'export_attendance_sheet'),
-    url(r'^export_feedback_sheet/(\w+)/(\d{4})/(\d{1})/(\w+)$', 'export_feedback_sheet', name = 'export_feedback_sheet'),
-    url(r'^export_marks/(\w+)/(\d+)/$', 'export_marks', name = 'export_marks'),
+urlpatterns += patterns(
+    'export.views',
+    url(
+        r'^export_all_anonymous_exam_marks/(\d+)/$',
+        'export_all_anonymous_exam_marks',
+        name='export_all_anonymous_exam_marks'
+        ),
+    url(
+        r'^export_anonymous_marks/(\w+)/(\d+)/(\w+)$',
+        'export_anonymous_marks',
+        name='export_anonymous_marks'
+        ),
+    url(
+        r'^export_attendance_sheet/(\w+)/(\d+)/$',
+        'export_attendance_sheet',
+        name='export_attendance_sheet'
+        ),
+    url(
+        r'^export_feedback_sheet/(\w+)/(\d{4})/(\d{1})/(\w+)$',
+        'export_feedback_sheet',
+        name='export_feedback_sheet'
+        ),
+    url(
+        r'^export_marks/(\w+)/(\d+)/$',
+        'export_marks',
+        name='export_marks'
+        ),
 )
 
-urlpatterns += patterns('anonymous_marking.views',
-    url(r'^anonymous_marking_admin/$', 'anonymous_marking_admin', name = 'anonymous_marking_admin'),
-    url(r'^edit_anon_ids/$', 'edit_anon_ids', name = 'edit_anon_ids'),
-    url(r'^mark_anonymously/(\w+)/(\d{4})/(\w+)/$', 'mark_anonymously', name = 'mark_anonymously'),
-    url(r'^de_anonymize/(\w+)/(\d{4})/(\w+)/$', 'de_anonymize', name = 'de_anonymize'),
-    url(r'^upload_anon_ids/$', 'upload_anon_ids', name = 'upload_anon_ids'),
+urlpatterns += patterns(
+    'anonymous_marking.views',
+    url(
+        r'^anonymous_marking_admin/$',
+        'anonymous_marking_admin',
+        name='anonymous_marking_admin'
+        ),
+    url(
+        r'^edit_anon_ids/$',
+        'edit_anon_ids',
+        name='edit_anon_ids'
+        ),
+    url(
+        r'^mark_anonymously/(\w+)/(\d{4})/(\w+)/$',
+        'mark_anonymously',
+        name='mark_anonymously'
+        ),
+    url(
+        r'^de_anonymize/(\w+)/(\d{4})/(\w+)/$',
+        'de_anonymize',
+        name='de_anonymize'
+        ),
+    url(
+        r'^upload_anon_ids/$',
+        'upload_anon_ids',
+        name='upload_anon_ids'
+        ),
 )
 
-urlpatterns += patterns('feedback.views',
-    url(r'^essay_feedback/(\w+)/(\d{4})/(\d{1})/(\w+)$', 'edit_feedback', name = 'edit_feedback'),
-    #    url(r'^essay_feedback/$', 'add_essay_feedback', name = 'add_essay_feedback')
+urlpatterns += patterns(
+    'feedback.views',
+    url(
+        r'^essay_feedback/(\w+)/(\d{4})/(\d{1})/(\w+)$',
+        'edit_feedback',
+        name='edit_feedback'
+        ),
 )
 
-urlpatterns += patterns('scheduler.views',
-    url(r'^set_up_appointments/$', 'set_up_appointments', name = 'set_up_appointments')
+urlpatterns += patterns(
+    'scheduler.views',
+    url(
+        r'^set_up_appointments/$',
+        'set_up_appointments',
+        name='set_up_appointments'
+        )
 )
--- mysds/views.py
+++ mysds/views.py
@@ -9,6 +9,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from database.models import Student, Module, Course
 from database import functions
 
+
 def is_teacher(user):
     if user:
         if user.groups.filter(name='teachers').count() == 1:
@@ -18,66 +19,82 @@ def is_teacher(user):
     else:
         return False
 
+
 @login_required
 @user_passes_test(is_teacher)
 def default_page(request):
     title = 'CCCU Law DB'
-    return render_to_response('home.html',
-            {},
-            context_instance = RequestContext(request)
-            )
+    return render_to_response(
+        'home.html',
+        {},
+        context_instance=RequestContext(request)
+        )
+
 
 def reset_password(request):
     if 'email' in request.GET and request.GET['email']:
         email = request.GET['email']
         try:
-            user = User.objects.get(email = email)
+            user = User.objects.get(email=email)
             new_password = User.objects.make_random_password()
             user.set_password(new_password)
             user.save()
-            first_name = user.first_name.split(" ")[0] # only first forename
+            first_name = user.first_name.split(" ")[0]  # only first forename
             message = """
-            Dear %s,
-            
-    You have requested your details from MySDS, the CCCU database. Here they are:
+Dear %s,
 
-    Username: %s
-    Password: %s
+You have requested your details from MySDS, the CCCU database. Here they are:
 
-    Please be aware that the form is case sensitive, so make sure you use upper- and lowercase correctly.
+Username: %s
+Password: %s
 
-    After successfully logging in with the above details, please click on "My Account" and change your password to one that is both easy to remember and difficult to guess. Make sure you do not use the same password for many different websites. A good way to ensure password safety and convenience is to use a password manager like KeepassX, Last Pass or One Password. All of them are available for almost all browsers and platforms.
+Please be aware that the form is case sensitive, so make sure you use upper-
+and lowercase correctly.
 
-    Best wishes,
+After successfully logging in with the above details, please click on
+"My Account" and change your password to one that is both easy to remember
+and difficult to guess. Make sure you do not use the same password for many
+different websites. A good way to ensure password safety and convenience is
+to use a password manager like KeepassX, Last Pass or One Password. All of
+them are available for almost all browsers and platforms.
 
-    Your friendly MySDS Admin
-            """%(first_name, user.username, new_password)
+Best wishes,
 
-            send_mail('MySDS - New login information', message, 'cccu@tobiaskliem.de', [user.email,])
+Your friendly MySDS Admin
+            """ % (first_name, user.username, new_password)
+
+            send_mail(
+                'MySDS - New login information',
+                message,
+                'cccu@tobiaskliem.de',
+                [user.email, ]
+                )
         except User.DoesNotExist:
             return HttpResponseRedirect('/wrong_email')
-#        print message #Just for local testing (no SMTP server on this machine)
+# For local testing:
+#        print message
 
     return HttpResponseRedirect('/')
+
 
 @login_required
 def invite_students(request):
     if request.method == 'POST':
         student_ids = request.POST.getlist('selected_student_id')
-        student_group = Group.objects.get(name = 'students')
+        student_group = Group.objects.get(name='students')
         for student_id in student_ids:
-            student = Student.objects.get(student_id = student_id)
+            student = Student.objects.get(student_id=student_id)
             first_part = student.first_name[0] + student.last_name[0]
             first_part = first_part.lower()
             number = 1
             still_searching = True
             while still_searching:
                 username = first_part + str(number)
-                if User.objects.filter(username = username).exists():
+                if User.objects.filter(username=username).exists():
                     number += 1
                 else:
                     still_searching = False
-            user = User(username = username, email = student.email)
+            user = User(username=username, email=student.email)
             user.save()
             password = User.objects.make_random_password()
             user.set_password(password)
@@ -86,20 +103,24 @@ def invite_students(request):
             student.save()
             student_group.user_set.add(user)
             student_group.save()
-            first_name = student.first_name.split(" ")[0] # only first forename
+            first_name = student.first_name.split(" ")[0]
             message = """
 Dear %s,
 
-You now have access to MySDS, the Student Database for Law at Canterbury Christ Church University.
+You now have access to MySDS, the Student Database for Law at Canterbury
+Christ Church University.
 
-The database is available at https://cccu.tobiaskliem.de, and you can access it with the following details:
+The database is available at https://cccu.tobiaskliem.de, and you can access
+it with the following details:
 
 Username: %s
 Password: %s
 
 When you log in, you can change your password if you click on 'My Account'.
 
-On the database, you will be able to see your marks, download your marksheets and book meetings with teachers. The first marksheets should be up soon, and your teachers will let you know about this.
+On the database, you will be able to see your marks, download your marksheets
+and book meetings with teachers. The first marksheets should be up soon, and
+your teachers will let you know about this.
 
 More functions are in planning.
 
@@ -108,37 +129,54 @@ If you experience problems, please contact cccu@tobiaskliem.de - thanks a lot!
 Enjoy the experience,
 
 Your friendly MySDS admin.
-"""%(first_name, user.username, password) 
-            send_mail('The Law Database at Canterbury Christ Church', message, 'cccu@tobiaskliem.de', [user.email,])
-            #print message #Just for local testing (no SMTP server on the testing machine)
+""" % (first_name, user.username, password)
+            send_mail(
+                'The Law Database at Canterbury Christ Church',
+                message,
+                'cccu@tobiaskliem.de',
+                [user.email, ]
+                )
+            # print message
         return HttpResponseRedirect('/')
     else:
         all_students = Student.objects.all()
-        students = [] 
+        students = []
         year = {}
         for student in all_students:
-            if student.belongs_to == None:
+            if student.belongs_to is None:
                 students.append(student)
                 year[student.year] = True
-        return render_to_response('invite_students.html',
-                {'students': students, 'year': year},
-                context_instance = RequestContext(request))
+        return render_to_response(
+            'invite_students.html',
+            {'students': students, 'year': year},
+            context_instance=RequestContext(request)
+            )
+
 
 def wrong_email(request):
-    printstring = '''<p>You did not provide a valid email address.</p>
-                    <p>
-                        Please make sure you use your CCCU email address - eg b.bunny123@canterbury.ac.uk.
-                    </p>
+    printstring = '''
+<p>You did not provide a valid email address.</p>
+<p>
+    Please make sure you use your CCCU email address
+    - eg b.bunny123@canterbury.ac.uk.
+</p>
 
-                    <a href="/" class="btn btn-primary">Please try again</a>'
-                '''
+<a href="/" class="btn btn-primary">Please try again</a>
+'''
     title = "CCCU Law DB - Wrong email"
 
-    return render_to_response('wrong_email.html', {'printstring': printstring, 'title': title}, context_instance = RequestContext(request))
-
+    return render_to_response(
+        'wrong_email.html',
+        {'printstring': printstring, 'title': title},
+        context_instance=RequestContext(request)
+        )
 
 
 def na(request):
     printstring = 'This function is not available yet'
     title = 'CCCU Law DB: Not available'
-    return render_to_response('blank.html', {'printstring': printstring, 'title': title}, context_instance = RequestContext(request))
+    return render_to_response(
+        'blank.html',
+        {'printstring': printstring, 'title': title},
+        context_instance=RequestContext(request)
+        )
